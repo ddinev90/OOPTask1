5c7f9a2db2cdabeda97a7cc07a75440b0be99754
Daniel Dinev <daniel.dinev@scalefocus.com>
2018-01-23 15:40:22 +0200

    Code refacturing and some fixes in Elipse

--- OOPTask/Elipse.cs
+++ OOPTask/Elipse.cs
@@ -4,35 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace OOPTask
 {
     class Elipse : IShape
     {
         public int[] fA { get; set; }
         public int[] fB { get; set; }
-        public int[] sumFoci { get; set; }
+        public double[] focalLength { get; set; }
        
 
         public void Draw()
         {
             Console.WriteLine("My shape is Elipse");
-            Console.WriteLine("Distance between foci is:");
 
-            foreach (var item in sumFoci)
-            {
-                Console.WriteLine(item);
-            }
             
-            
+
         }
 
         public Elipse(int[] a, int[] b)
         {
             this.fA = a;
             this.fB = b;
-            sumFoci = a.Select((x, index) => x + b[index]).ToArray();
+            
 
         }
+
+        public void FocalLength()
+        {
+            focalLength = fA.Select((x, index) => Math.Sqrt(Math.Pow(x, 2) - Math.Pow(fB[index], 2))).ToArray();
+            double length = 0;
+            for (int i = 0; i < focalLength.Length; i++)
+            {
+                length += focalLength[i];
+
+
+            }
+
+            Console.WriteLine("Distance between foci is: {0}", length);
+
+        }
+
     }
 }
