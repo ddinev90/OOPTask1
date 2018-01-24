3f9ff56c91161f6698658c21b38ecfa22619dabe
Daniel Dinev <daniel.dinev@scalefocus.com>
2018-01-24 16:26:33 +0200

    Added the most brutal way of checking what a shape is. (jk)

--- OOPTask/Parallelogram.cs
+++ OOPTask/Parallelogram.cs
@@ -11,9 +11,9 @@ namespace OOPTask
 
         public Parallelogram(double aX, double aY, double bX, double bY, double cX, double cY, double dX, double dY) : base(aX, aY, bX, bY, cX, cY, dX, dY)
         {
-            dX = cX - bX + aX;
+            /* dX = cX - bX + aX;
             dY = cY - bY + aY;
-            Console.Write("The missing vertice X coord is {0}" + " and the missing Y coord is {1} ",dX,dY, Environment.NewLine);
+            Console.Write("The missing vertice X coord is {0}" + " and the missing Y coord is {1} ",dX,dY, Environment.NewLine); */
             
         }
     }
--- OOPTask/Program.cs
+++ OOPTask/Program.cs
@@ -12,31 +12,68 @@ namespace OOPTask
         {
 
 
+            double pointAx, pointAy, pointBx, pointBy, pointCx, pointCy, pointDx, pointDy;
 
-            Quadrangle quadrangle = new Quadrangle(4,6,2,3,2,5,3,4);
+            Console.WriteLine("Enter the X and Y of the points defining a quadrangle :");
+
+            pointAx = double.Parse(Console.ReadLine().ToString());
+            pointAy = double.Parse(Console.ReadLine().ToString());
+            pointBx = double.Parse(Console.ReadLine().ToString());
+            pointBy = double.Parse(Console.ReadLine().ToString());
+            pointCx = double.Parse(Console.ReadLine().ToString());
+            pointCy = double.Parse(Console.ReadLine().ToString());
+            pointDx = double.Parse(Console.ReadLine().ToString());
+            pointDy = double.Parse(Console.ReadLine().ToString());
+
+
+             double slopeAB = (pointBy - pointAy) / (pointBx - pointAx);
+             double slopeCD = (pointDy - pointCy) / (pointDx - pointCx);
+             double slopeAD = (pointDy - pointAy) / (pointDx - pointAx);
+             double slopeBC = (pointCy - pointBy) / (pointCx - pointBx);
+             double slopeAC = (pointCy - pointAy) / (pointCx - pointAx);
+             double slopeBD = (pointDy - pointBy) / (pointDx - pointBx);
+
+           
+
+
+
+
+
+
+            if(slopeAB==slopeCD && slopeAD==slopeBC) //parallelogram
+             {
+
+                if(slopeAB == slopeCD && slopeAD == slopeBC && slopeAB * slopeAD == -1) // rectangle
+                {
+                    if(slopeAB == slopeCD && slopeAD == slopeBC && slopeAB * slopeAD == -1 && slopeAC * slopeBD == -1) //square
+                    {
+                        Square squicky = new Square(pointAx, pointAy, pointBx, pointBy, pointCx, pointCy, pointDx, pointDy);
+                        squicky.Draw();
+                        return;
+                    }
+                    Rectangle rect = new Rectangle(pointAx, pointAy, pointBx, pointBy, pointCx, pointCy, pointDx, pointDy);
+                    rect.Draw();
+                    return;
+
+                }
+                
+                if (slopeAB == slopeCD && slopeAD == slopeBC && slopeAC * slopeBD == -1)
+                {
+                    Rhombus rhom = new Rhombus(pointAx, pointAy, pointBx, pointBy, pointCx, pointCy, pointDx, pointDy);
+                    rhom.Draw();
+                    return;
+                }
+                Parallelogram para = new Parallelogram(pointAx, pointAy, pointBx, pointBy, pointCx, pointCy, pointDx, pointDy);
+                para.Draw();
+                return;
+
+            } else// random quadrangle
+             {
+                 Quadrangle quady = new Quadrangle(pointAx, pointAy, pointBx, pointBy, pointCx, pointCy, pointDx, pointDy);
+                 quady.Draw();
+             }
 
             
-
-            quadrangle.Draw();
-
-            Elipse elipse = new Elipse(15, 20, 30, 10);
-
-            elipse.Draw();
-
-            Circle circle = new Circle(5,2,3,5);
-
-            circle.Draw();
-
-            elipse.FocalLength();
-            circle.SurfaceArea();
-
-            Parallelogram parallelogram = new Parallelogram(4, 3, 2, 4, 5, 6, 0, 0);
-
-            Square square = new Square(3, 4, 5, 6, 7, 8, 0, 0);
-            
-
-
-            Console.ReadKey();
         }
     }
 }
